bbf4bcd41ecea2f1bcdd89d8d96019c28db0869f
N-Koyama <112364906+Nz-Koyama@users.noreply.github.com>
2026-08-18 13:32:00 +0900

    Revert "社名変更に伴う細かい修正 (#8)"
    This reverts commit dc73ce19d2c44559c5f70cf880c1026e818466d1.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ import { InstagramLogo } from "./InstagramLogo";
 const socialLinks = [
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/feed/update/urn:li:activity:7495083231109730304/?actorCompanyId=105150520",
+    href: "https://www.linkedin.com/company/gows-inc",
     icon: (
       <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
         <path d="M20.45 20.45h-3.55v-5.57c0-1.33-.02-3.04-1.85-3.04-1.85 0-2.13 1.45-2.13 2.94v5.67H9.36V9h3.41v1.56h.05c.47-.9 1.64-1.85 3.37-1.85 3.61 0 4.27 2.37 4.27 5.46v6.28zM5.34 7.43a2.06 2.06 0 1 1 0-4.12 2.06 2.06 0 0 1 0 4.12zM7.12 20.45H3.56V9h3.56v11.45zM22.22 0H1.77C.79 0 0 .77 0 1.72v20.56C0 23.23.79 24 1.77 24h20.45c.98 0 1.78-.77 1.78-1.72V1.72C24 .77 23.2 0 22.22 0z" />
@@ -15,12 +15,12 @@ const socialLinks = [
   },
   {
     name: "X",
-    href: "https://x.com/pitdock",
+    href: "https://x.com/gows_ktma",
     icon: <XLogo />,
   },
   {
     name: "Instagram",
-    href: "https://www.instagram.com/pitdock_official/",
+    href: "https://www.instagram.com/gows_ktma/",
     icon: <InstagramLogo />,
   },
   {
